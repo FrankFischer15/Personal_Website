6453cd445ae197b1a02ddfb33038c4c19bd9ac55
Frank <Frank_Fischer_15@yahoo.com>
2015-03-27 15:19:19 -0400

    File Structure

---
+++ app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name personalWebsiteApp
+ * @description
+ * # personalWebsiteApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('personalWebsiteApp', [
+    'ngAnimate',
+    'ngResource',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
